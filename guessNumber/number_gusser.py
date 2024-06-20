886bd5050431e05894e50e8bd848996268358043
dumbDeveloper <160309524+shivDroid@users.noreply.github.com>
2024-06-20 03:23:09 +0000

    Add number of attempts in number guess

--- guessNumber/number_gusser.py
+++ guessNumber/number_gusser.py
@@ -3,15 +3,20 @@ import random,sys
 sys_guess = random.randint(1,20)
 print("I am thinking of a number between 1 and 20, take a guess?")
 
+number_of_guess = 0 
 while True: 
     user_guess = int(input("Enter your guess:  "))
+    number_of_guess = number_of_guess + 1
     if user_guess == sys_guess:
+        print(f"Wohooooo! Congratulations you guessed the number right. It took you {number_of_guess} attempts")
         sys.exit()
     elif user_guess - sys_guess == -4 or user_guess - sys_guess == 4:
         print("You are close!!!") 
     elif user_guess < sys_guess:
         print("Your guess is lower than mine")
     else:
-        print("Your guess is higher than mine")  
+        print("Your guess is higher than mine") 
+    
+         
 
 
